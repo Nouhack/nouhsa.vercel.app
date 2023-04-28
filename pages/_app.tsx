d4276ec8ebb96ce29a25351d667544b55259d229
Nouhack <nouh.saiche@gmail.com>
2023-04-28 21:11:01 +0100

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
 import "highlight.js/styles/atom-one-dark.css";
+import {Analytics} from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="container">
       <Header />
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
