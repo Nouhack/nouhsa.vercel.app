0e913fa6d12395cd3f1d4f05f36e7fd46ad55ef9
Nouhack <nouh.saiche@gmail.com>
2023-08-04 22:32:52 +0100

    add new blog

--- posts.js
+++ posts.js
@@ -1,7 +1,12 @@
 export const Posts = [
-  {
-    title: "How did i rebuild my portfolio",
-    date: "2023-03-20",
-    slug: "/rebuild_my_portfolio",
-  },
+    {
+        title: "How did i rebuild my portfolio",
+        date: "2023-03-20",
+        slug: "/rebuild_my_portfolio",
+    },
+    {
+        title: "How to stay motivated when learning new things",
+        date: "2023-08-04",
+        slug: "/stay_motivated_when_learning_new_things",
+    },
 ];
